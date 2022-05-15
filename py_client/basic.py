8406e80a2854eebf82ba7e93d7718942ef30d006
Santi <104403779+santisdev@users.noreply.github.com>
2022-05-15 15:42:00 -0300

    first view

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,10 @@
 from turtle import end_poly
 import requests
 
-endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/status/200/"
+endpoint = "http://localhost:8000/api/"
 
 get_restponse = requests.get(endpoint, json={"query":"hello world"})
-print(get_restponse.text)
+# print(get_restponse.text)
 print(get_restponse.json())
+print(get_restponse.json()['message'])
