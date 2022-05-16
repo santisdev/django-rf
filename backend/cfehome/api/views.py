788b54a2449a21619fb0b6b8847482fbd6e9eac2
Santi <104403779+santisdev@users.noreply.github.com>
2022-05-15 23:01:09 -0300

    first model, first product

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,18 +1,13 @@
 import json
 from django.http import JsonResponse
+from products.models import Product
 
 def api_home (request, *args, **kwargs):
-  print(request.GET) # url query params
-  body = request.body  #JSON byte string body
+  model_data = Product.objects.all().order_by('?').first()
   data = {}
-  try:
-    data = json.loads(body) # string of jsondata -> python dictionary
-  except:
-    pass
-  print(data)
-  data['params'] = dict(request.GET)
-  data['headers'] = dict(request.headers)
-  data['content_type'] = request.content_type
+  if model_data:
+    data['id'] = model_data.id
+    data['title'] = model_data.title
+    data['content'] = model_data.content
+    data['price'] = model_data.price
   return JsonResponse(data)
-
- 
