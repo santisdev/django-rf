a1503a454308ad8970eeb364004e42d93f9668ef
Santi <104403779+santisdev@users.noreply.github.com>
2022-05-15 22:43:24 -0300

    end Echo data

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,5 +1,18 @@
+import json
 from django.http import JsonResponse
 
 def api_home (request, *args, **kwargs):
-  return JsonResponse({"message": "Hi there this is your api response"})
+  print(request.GET) # url query params
+  body = request.body  #JSON byte string body
+  data = {}
+  try:
+    data = json.loads(body) # string of jsondata -> python dictionary
+  except:
+    pass
+  print(data)
+  data['params'] = dict(request.GET)
+  data['headers'] = dict(request.headers)
+  data['content_type'] = request.content_type
+  return JsonResponse(data)
 
+ 
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,6 @@ import requests
 # endpoint = "https://httpbin.org/status/200/"
 endpoint = "http://localhost:8000/api/"
 
-get_restponse = requests.get(endpoint, json={"query":"hello world"})
+get_restponse = requests.get(endpoint, params={"abc":123}, json={"query":"hello world"})
 # print(get_restponse.text)
 print(get_restponse.json())
-print(get_restponse.json()['message'])
