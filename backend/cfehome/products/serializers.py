1dd66130ad5b73c5365f0be05636e3be9f3bc6a2
Santi <104403779+santisdev@users.noreply.github.com>
2022-05-21 15:09:47 -0300

    Model serializer and change field name

--- backend/cfehome/products/serializers.py
+++ backend/cfehome/products/serializers.py
@@ -2,13 +2,16 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+  my_discount = serializers.SerializerMethodField(read_only=True)
   class Meta:
-    # discount = serializers.SerializerMethodField(read_only=True)
     model = Product
     fields = [
       'title',
       'content',
       'price',
       'sale_price',
-      'get_discount'
+      'my_discount',
     ]
+
+  def get_my_discount(self, obj):
+    return obj.get_discount()
