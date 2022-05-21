b7910677fa871c37f17cb47dadef38d195c673ce
Santi <104403779+santisdev@users.noreply.github.com>
2022-05-21 19:21:30 -0300

    Saving product with serializer

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -6,14 +6,13 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from products.serializers import ProductSerializer
 
-@api_view(['GET'])
+@api_view(['POST'])
 def api_home (request, *args, **kwargs):
   """
   DRF API View
   """
-  instance = Product.objects.all().order_by('?').first()
-  data = {}
-  if instance:
-    # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
-    data = ProductSerializer(instance).data
-  return Response(data)
+  serializer = ProductSerializer(data = request.data)
+  if serializer.is_valid():
+    instance = serializer.save()
+    print(instance)
+  return Response(serializer.data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,6 +4,6 @@ import requests
 # endpoint = "https://httpbin.org/status/200/"
 endpoint = "http://localhost:8000/api/"
 
-get_restponse = requests.get(endpoint, params={"abc":123}, json={"query":"hello world"})
+get_restponse = requests.post(endpoint, json={"title":"hello world"})
 # print(get_restponse.text)
 print(get_restponse.json())
