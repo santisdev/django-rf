d98b4ff2099953e1ac05fd12eb4934f70931ecda
Santi <104403779+santisdev@users.noreply.github.com>
2022-06-01 17:58:58 -0300

    Adding comments

--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -3,7 +3,12 @@ from .models import Product
 from .serializers import ProductSerializer
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
+  #gets one single item
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
   # lookup_field = 'pk'
+
+
+  # if you want to change the query set
+  # def get_queryset():
 
